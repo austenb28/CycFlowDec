7ceae20fe5ed64aae06acc3bc0369e9ef6c57d55
austenb28 <austenb28@gmail.com>
2020-08-20 14:05:10 -0600

    Update example.py

--- example.py
+++ example.py
@@ -22,20 +22,23 @@ MREs = [myCycFlowDec.calc_MRE(0)]
 
 j = 1
 while j < n_points:
-  myCycFlowDec.run(step_add-run_post,run_post)
-  steps.append(steps[-1] + step_add)
-  MREs.append(myCycFlowDec.calc_MRE(0))
-  j += 1
+	myCycFlowDec.run(step_add-run_post,run_post)
+	steps.append(steps[-1] + step_add)
+	MREs.append(myCycFlowDec.calc_MRE(0))
+	j += 1
 
-print('Cycle   Flow')
-for cycle in myCycFlowDec.keys():
-  print('{:s}: {:.2f}'.format(
-    str(cycle),
-    myCycFlowDec.cycles[cycle]
-  )
+print('{:>11s} {:>6s}'.format(
+	'Cycle',
+	'Flow'
+))
+for cycle in myCycFlowDec.cycles.keys():
+	print('{:>11s} {:>6.2f}'.format(
+		str(cycle),
+		myCycFlowDec.cycles[cycle]
+	))
 
-plt.xlabel("Steps")
-plt.ylabel("MRE")
+plt.xlabel('Steps')
+plt.ylabel('MRE')
 plt.plot(steps,MREs,'bo')
 plt.yscale('log')
 plt.show()
